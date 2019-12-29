c4ff1f62fada7e90a958ad4e8a8b3c9cdd2350e6
Fatem Ataee <fatemehataee95@gmail.com>
2019-12-29 13:12:32 +0330

    initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+window.onscroll = function() {stickyHeader()};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    item:1,
+    merge:true,
+    loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1,
+        }
+    }
+  });
+});
